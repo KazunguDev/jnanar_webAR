a3754f5f4c9cab02bd1882bac1fa178bcee49e4a
KazunguDev <kazungudev@gmail.com>
2023-09-12 15:12:19 +0300

    build folder

---
+++ dist/mindar-image.prod.js
@@ -0,0 +1,13 @@
+import { C as o, a as r } from "./controller-ade172fc.js";
+import { U as i } from "./ui-d00b5250.js";
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.IMAGE = {
+  Controller: o,
+  Compiler: r,
+  UI: i
+};
+export {
+  r as Compiler,
+  o as Controller,
+  i as UI
+};
